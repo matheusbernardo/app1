f53e556cd13f0081b748cb9cb45d5ac072cfd949
Matheus Bernardo Debossan de Abreu <matheusbernardo.debossan@gmail.com>
2018-09-25 20:38:50 -0300

    Commit referente ao conteúdo estudado até a aula 67

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -9,20 +9,41 @@ import { Frase } from "../shared/frase.model";
   styleUrls: ['./painel.component.css']
 })
 export class PainelComponent implements OnInit {
-
+  public resposta:string = ""
   public frases: Array<Frase> = FRASES
   public instrucao:string = 'Traduza a frase:'
+  public rodada:number = 0
+  public rodadaFrase : Frase
+  public progresso:number = 0
 
-  constructor() { console.log(this.frases) }
+  constructor() {
+    this.rodadaFrase = this.frases[this.rodada]
+   }
 
   ngOnInit() {
   }
 
-  /**
-   * testaEventBinding
-   */
-  public testaEventBinding() {
-    console.log("Evento capturado")
+  public atualizaResposta(resposta : Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value
+    console.log(this.resposta)    
+  }
+
+  public verificarResposta() :void {
+    if (this.rodadaFrase.frasePtBr == this.resposta) {
+      if (this.rodada < this.frases.length-1) {
+        this.rodada++
+        this.rodadaFrase = this.frases[ this.rodada ]
+        this.progresso = this.progresso + 25
+        this.resposta = ""
+        alert("A frase está correta!")
+      } else {
+        this.progresso = this.progresso + 25
+        this.resposta = ""
+        alert("A frase está correta!")
+      }
+    } else {
+      alert("A frase está errada")
+    }    
   }
 
 }
